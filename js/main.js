463cabba76695f9f22f770908a0dee85cb8a565b
boburDev <negmatov1907@gmail.com>
2020-06-12 23:14:26 +0500

    update js file

--- js/main.js
+++ js/main.js
@@ -2,36 +2,26 @@
   	once: true,
   	delay: 500,
   });
+  
   let scroll = new SmoothScroll('a[href*="#"]');
 
   let openNavButtonElement = document.querySelector('.navbar-toggler')
   let navListElement = document.querySelector('.navbar_list')
+ 
+  let addClass = function() {
+  	navListElement.classList.add('navbar--open');
+  	openNavButtonElement.addEventListener('click', onCloseNavListClick);
+  };
+  let removeClass = function() {
+  	navListElement.classList.remove('navbar--open');
+  	openNavButtonElement.removeEventListener('click', onCloseNavListClick);
+  }
 
+  let onOpenNavListButtonClick = function () {
+  	addClass();
+  }
+  let onCloseNavListClick = function () {
+  	removeClass();
+  }
 
-var onNavListClick = function (evt) {
-	if(evt.target.matches('.navbar_list')){
-		closeModal();
-	}
-};
-
-var addClass = function() {
-	navListElement.classList.add('navbar--open');
-
-	openNavButtonElement.addEventListener('click', onCloseNavListClick);
-	navListElement.addEventListener('click',onNavListClick);
-};
-var removeClass = function() {
-	navListElement.classList.remove('navbar--open');
-
-	openNavButtonElement.removeEventListener('click', onCloseNavListClick);
-	navListElement.remoteEventListener('click',onNavListClick);
-}
-
-var onOpenNavListButtonClick = function () {
-	addClass();
-}
-var onCloseNavListClick = function () {
-	removeClass();
-}
-
-openNavButtonElement.addEventListener('click', onOpenNavListButtonClick);
+  openNavButtonElement.addEventListener('click', onOpenNavListButtonClick);
